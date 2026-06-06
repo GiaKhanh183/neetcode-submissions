4c8dea87a9d34925b88319c7d28fab3303844073
GiaKhanh183 <khanhhuynh1837@gmail.com>
2026-06-06 12:12:23 +0700

    Add: string-encode-and-decode - submission-32

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-32.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        sentence = ""
+        for strings in strs:
+            if strings == "":
+                sentence = sentence + 'a' + strings
+            else:
+                sentence = sentence + chr(ord('a')+len(strings)+1) + strings
+        return sentence
+    def decode(self, s: str) -> List[str]:
+        res = []
+        cursor = int(0)
+        step = int(0)
+        
+        while cursor < len(s):
+            step = ord(s[cursor])-ord('a')
+            if step == 0:
+                res.append("")
+                cursor += 1
+            else:
+                res.append(s[(cursor+1):(cursor+step)])
+                cursor += step
+        return res
